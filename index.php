0c2b04d5720fbdbc41a5b5413495cd6abd35f5c5
YAMMOUNII <elie.yammouny@hotmail.com>
2022-05-08 21:21:34 +0300

    add 4 col

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 
 <div class="container-fluide">
     <div class="row">
-        <div class="col"></div>
-        <div class="col"></div>
-        <div class="col"></div>
-        <div class="col"></div>
+        <div class="col">1</div>
+        <div class="col">2</div>
+        <div class="col">3</div>
+        <div class="col">4</div>
+        <div class="col">1</div>
+        <div class="col">2</div>
+        <div class="col">3</div>
+        <div class="col">4</div>
     </div>
 </div>
