72cee0d4a5af2c39bddcbb44d807be3a995b91e6
YAMMOUNII <elie.yammouny@hotmail.com>
2022-05-09 20:54:29 +0300

    remove 4 collomum bub

--- index.php
+++ index.php
@@ -6,9 +6,5 @@
         <div class="col">2</div>
         <div class="col">3</div>
         <div class="col">4</div>
-        <div class="col">1</div>
-        <div class="col">2</div>
-        <div class="col">3</div>
-        <div class="col">4</div>
     </div>
 </div>
